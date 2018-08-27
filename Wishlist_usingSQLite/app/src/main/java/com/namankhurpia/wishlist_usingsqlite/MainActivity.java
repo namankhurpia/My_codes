25f885ad8be4d0f796d53aca0b4a105a0149c17a
namankhurpia <naman.khurpia2016@vitstudent.ac.in>
2018-08-27 20:28:14 +0530

    WishList using SQLite
    WishList using SQLite- SCA

---
+++ Wishlist_usingSQLite/app/src/main/java/com/namankhurpia/wishlist_usingsqlite/MainActivity.java
@@ -0,0 +1,13 @@
+package com.namankhurpia.wishlist_usingsqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
